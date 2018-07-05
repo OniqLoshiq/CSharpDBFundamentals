5900cc7970be49d86ebc5464d70a5561f30bc920
OniqLoshiq <doncho.vasilev@gmail.com>
2018-07-05 20:53:44 +0300

    C# DB Advanced - Ex3 EntityFramework Core Intro

---
+++ 08- 180626-C# DB Advanced/180703-DBA-7 Ex 3 - EF Intro/180703-DBA-Ex3/10-DepsWithMoreThan5Empl/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P02_DatabaseFirst.Data;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace _10_DepsWithMoreThan5Empl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SoftUniContext())
+            {
+                var deps = ctx.Departments.Include(d => d.Employees)
+                                          .Where(d => d.Employees.Count > 5)
+                                          .OrderBy(d => d.Employees.Count)
+                                          .ThenBy(d => d.Name)
+                                          .Select(d => new
+                                          {
+                                              DepartmentName = d.Name,
+                                              DepartmentManager = d.Manager.FirstName + " " + d.Manager.LastName,
+                                              Employees = d.Employees.Select(e => new
+                                                                     {
+                                                                         e.FirstName,
+                                                                         e.LastName,
+                                                                         e.JobTitle
+                                                                     })
+                                                                     .OrderBy(e => e.FirstName)
+                                                                     .ThenBy(e => e.LastName)
+                                          })
+                                          .ToArray();
+                
+                using (StreamWriter sw = new StreamWriter("../../../../10_DepsWithMoreThan5Empl.txt"))
+                {
+                    foreach (var d in deps)
+                    {
+                        sw.WriteLine($"{d.DepartmentName} - {d.DepartmentManager}");
+
+                        foreach (var e in d.Employees)
+                        {
+                            sw.WriteLine($"{e.FirstName} {e.LastName} - {e.JobTitle}");
+                        }
+
+                        sw.WriteLine("----------");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 08- 180626-C# DB Advanced/180703-DBA-7 Ex 3 - EF Intro/180703-DBA-Ex3/14-DeleteProjectById/Program.cs
@@ -0,0 +1,36 @@
+﻿using P02_DatabaseFirst.Data;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace _14_DeleteProjectById
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idToDelete = 2;
+
+            using (var ctx = new SoftUniContext())
+            {
+                var projects = ctx.EmployeesProjects.Where(p => p.ProjectId == idToDelete);
+                ctx.EmployeesProjects.RemoveRange(projects);
+
+                var project = ctx.Projects.Find(idToDelete);
+                ctx.Projects.Remove(project);
+
+                ctx.SaveChanges();
+
+                var projectsToPrint = ctx.Projects.Select(p => p.Name).Take(10);
+
+                using (StreamWriter sw = new StreamWriter("../../../../14_DeleteProjectById.txt"))
+                {
+                    foreach (var p in projectsToPrint)
+                    {
+                        sw.WriteLine(p);
+                    }
+                }
+            }
+        }
+    }
+}
